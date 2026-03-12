90bb3d53b3bfafb70f5673c3f44aa3a11de832fe
Owenr0311 <owenrheaume@csu.fullerton.edu>
2026-03-12 19:46:57 +0000

    updated

--- Main.cc
+++ Main.cc
@@ -1,2 +1,23 @@
-Owen Rheaume
-841215908
+// Owen Rheaume
+// 841215908
+
+ #include <string>
+ #include <iostream>
+
+void double_it(int* x) {
+    *x = *x * 2;
+    std::cout << "x is " << *x << "\n";
+}
+
+void double_it_pointer(int* x) {
+    *(x + 1) = *(x + 1) * 2;
+    std::cout << "x is " << *x << "\n";
+}
+
+int main() {
+    int value [] = {5, 7, 13, 17, 19};
+    std::cout << "value is " << value[0] << "\n";
+    double_it_pointer(value);
+    std::cout << "value is " << *value << "\n";
+    return 0;
+}
